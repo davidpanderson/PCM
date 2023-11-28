e003778d0027cf770f69606fe8461b6e8e629e7d
David Anderson <davea@ssl.berkeley.edu>
2023-11-28 01:35:18 -0800

    terms:     'group' -> 'team'     'group set' -> 'league'     'storage_outer' -> 'outer_storage' change pointers to references

--- pcm.cpp
+++ pcm.cpp
@@ -1,3 +1,5 @@
+// part of a 'parallel computing manager'
+
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
@@ -8,42 +10,42 @@ using std::vector;
 
 struct HOST {
     double flops;
-    double storage_outer;
-    double storage_inner;
+    double outer_storage;
+    double inner_storage;
     int id;
     HOST(double f, double so, double si, int i) {
         flops = f;
-        storage_outer = so;
-        storage_inner = si;
+        outer_storage = so;
+        inner_storage = si;
         id = i;
     }
 };
 
 struct JOB_PARAMS {
-    int max_hosts_per_group;
-    int max_groups;
+    int max_hosts_per_team;
+    int max_teams;
     double max_var_hosts;
-        // max fastest/slowest ratio within a group
-    double max_var_groups;
-        // max fastest/slowest ratio of total flops between groups
+        // max fastest/slowest ratio within a team
+    double max_var_teams;
+        // max fastest/slowest ratio of total flops between teams
     double size_outer;
-        // data to be divided among groups
+        // data to be divided among teams
     double size_inner;
-        // data to be divided within a group
+        // data to be divided within a team
     bool aggressive;
-        // if true, allow a group to exceed the max_var_groups limit
+        // if true, allow a team to exceed the max_var_teams limit
         // rather than skipping a host.
         // This optimized the job possibly at the expense of later jobs
 };
 
-// describes a group of (similar) hosts
+// describes a set of (similar) hosts
 //
-struct GROUP {
+struct TEAM {
     vector<HOST*> hosts;
-    double max_flops;       // speed of fastest host in group
+    double max_flops;       // speed of fastest host in team
     double total_flops;     // sum of host flops
 
-    GROUP() {
+    TEAM() {
         max_flops = 0;
         total_flops = 0;
     }
@@ -62,11 +64,11 @@ struct GROUP {
         max_flops = 0;
         total_flops = 0;
     }
-    void add_host(HOST *h) {
-        hosts.push_back(h);
-        total_flops += h->flops;
-        if (h->flops > max_flops) {
-            max_flops = h->flops;
+    void add_host(HOST &h) {
+        hosts.push_back(&h);
+        total_flops += h.flops;
+        if (h.flops > max_flops) {
+            max_flops = h.flops;
         }
     }
     double mean_flops() {
@@ -79,7 +81,7 @@ struct GROUP {
             printf("   %d: %f\n", hosts[i]->id, hosts[i]->flops);
         }
     }
-    // Calculate the inner storage needed, based on group size.
+    // Calculate the inner storage needed, based on team size.
     // Remove hosts that don't have that much.
     // Return true if removed any.
     //
@@ -89,7 +91,7 @@ struct GROUP {
         vector<HOST*> new_hosts;
         bool found = false;
         for (HOST* h: hosts) {
-            if (h->storage_inner < needed) {
+            if (h->inner_storage < needed) {
                 found = true;
             } else {
                 new_hosts.push_back(h);
@@ -100,8 +102,8 @@ struct GROUP {
         return found;
     }
 
-    void remove(HOST* h) {
-        if (hosts.back() == h) {
+    void remove(HOST& h) {
+        if (hosts.back() == &h) {
             hosts.pop_back();
             update();
         }
@@ -120,108 +122,108 @@ struct GROUP {
     }
 };
 
-// describes a set of groups
+// describes a set of teams
 //
-struct GROUP_SET {
-    vector<GROUP*> groups;
+struct LEAGUE {
+    vector<TEAM*> teams;
     double total_flops;
     double flops0;
-    GROUP_SET() {
+    LEAGUE() {
         total_flops = 0;
     }
-    void add_group(GROUP g) {
-        if (groups.empty()) {
-            flops0 = g.total_flops;
+    void add_team(TEAM t) {
+        if (teams.empty()) {
+            flops0 = t.total_flops;
         }
-        GROUP *g2 = new GROUP;
-        *g2 = g;
-        groups.push_back(g2);
-        total_flops += g.total_flops;
+        TEAM *t2 = new TEAM;
+        *t2 = t;
+        teams.push_back(t2);
+        total_flops += t.total_flops;
     }
     void print() {
-        for (unsigned int i=0; i<groups.size(); i++) {
-            GROUP &g = *groups[i];
-            printf("group %d:\n", i);
-            g.print();
+        for (unsigned int i=0; i<teams.size(); i++) {
+            TEAM &t = *teams[i];
+            printf("team %d:\n", i);
+            t.print();
         }
     }
     void clear() {
-        groups.clear();
+        teams.clear();
         total_flops = 0;
         flops0 = 0;
     }
 };
 
-bool compare(HOST* p1, HOST* p2) {
-    return p1->flops > p2->flops;
+bool compare(HOST* h1, HOST* h2) {
+    return h1->flops > h2->flops;
 }
 
-// Find a group set given the constraints that
-// - the total FLOPS of a group can't exceed a given value.
-// - the storage_outer of hosts must be at least a given value
+// Find a league given the constraints that
+// - the total FLOPS of a team can't exceed a given value.
+// - the outer_storage of hosts must be at least a given value
 // ... as well as the job params constraints.
 //
 // Enforce the inner storage constraint as follows:
-// when about to accept a group, compute the inner storage req (Y/N).
+// when about to accept a team, compute the inner storage req (Y/N).
 // If any hosts don't satisfy it,
-// remove them from the group ('inner storage pruning') and continue scanning
+// remove them from the team ('inner storage pruning') and continue scanning
 //
-void find_group_set3(
-    vector<HOST*> &hosts, JOB_PARAMS &params, GROUP_SET &s,
-    double max_group_flops,
-    double min_storage_outer
+void find_league3(
+    vector<HOST*> &hosts, JOB_PARAMS &params, LEAGUE &league,
+    double max_team_flops,
+    double min_outer_storage
 ) {
-    GROUP c;        // in-progress candidate group
+    TEAM c;        // in-progress candidate team
 
 #ifdef DEBUG
     printf(
-        "find_group_set3() start: max_group_flops %f, min_storage_outer %f\n",
-        max_group_flops, min_storage_outer
+        "find_league3() start: max_team_flops %f, min_outer_storage %f\n",
+        max_team_flops, min_outer_storage
     );
 #endif
-    s.clear();
+    league.clear();
     for (unsigned int i=0; i<hosts.size(); i++) {
-        if (s.groups.size() == params.max_groups) {
+        if (league.teams.size() == params.max_teams) {
 #ifdef DEBUG
-            printf("reached max groups\n");
+            printf("reached max teams\n");
 #endif
             break;
         }
-        HOST* h = hosts[i];
+        HOST& h = *hosts[i];
 #ifdef DEBUG
-        printf("scan host %d: flops %f\n", h->id, h->flops);
+        printf("scan host %d: flops %f\n", h.id, h.flops);
 #endif
-        if (h->storage_outer < min_storage_outer) {
+        if (h.outer_storage < min_outer_storage) {
 #ifdef DEBUG
-            printf("insufficient outer storage %f\n", h->storage_outer);
+            printf("insufficient outer storage %f\n", h.outer_storage);
 #endif
             continue;
         }
-        if (s.groups.empty()) {
-            // no accepted groups so far.
-            // Try to add H to the candidate group C
-            // This is the first group, so we need to enforce max_group_flops
+        if (league.teams.empty()) {
+            // no accepted teams so far.
+            // Try to add H to the candidate team C
+            // This is the first team, so we need to enforce max_team_flops
             //
             // Loop because of inner storage pruning
             while (1) {
                 // If C is nonempty, see if H is too slow for it.
                 // If so, try to accept C
-                if (c.total_flops && h->flops < c.max_flops/params.max_var_hosts) {
+                if (c.total_flops && h.flops < c.max_flops/params.max_var_hosts) {
                     if (c.prune_inner_storage(params.size_inner)) {
                         continue;
                     }
 #ifdef DEBUG
-                    printf("add group 1\n");
+                    printf("add team 1\n");
                     c.print();
 #endif
-                    s.add_group(c);
+                    league.add_team(c);
                     c.clear();
                     c.add_host(h);
                     goto next_host;
                 }
                 // see if adding H would make C too fast
                 //
-                if (c.total_flops + h->flops > max_group_flops) {
+                if (c.total_flops + h.flops > max_team_flops) {
                     // If so, and C is nonempty, try to accept it
                     //
                     if (c.total_flops) {
@@ -229,13 +231,13 @@ void find_group_set3(
                             continue;
                         }
 #ifdef DEBUG
-                        printf("add group 2\n");
+                        printf("add team 2\n");
                         c.print();
 #endif
-                        s.add_group(c);
+                        league.add_team(c);
                         c.clear();
                     }
-                    // New candidate group is {H}
+                    // New candidate team is {H}
                     c.add_host(h);
                     goto next_host;
                 } else {
@@ -243,16 +245,16 @@ void find_group_set3(
                     // If C is now full, try to accept it
                     //
                     c.add_host(h);
-                    if (c.hosts.size() == params.max_hosts_per_group) {
+                    if (c.hosts.size() == params.max_hosts_per_team) {
                         if (c.prune_inner_storage(params.size_inner)) {
                             c.remove(h);
                             continue;
                         }
 #ifdef DEBUG
-                        printf("add group 3\n");
+                        printf("add team 3\n");
                         c.print();
 #endif
-                        s.add_group(c);
+                        league.add_team(c);
                         c.clear();
                         goto next_host;
                     } else {
@@ -262,16 +264,16 @@ void find_group_set3(
                 }
             }
         } else {
-            // There is at least 1 accepted group,
-            // so we have to enforce params.max_var_groups
+            // There is at least 1 accepted team,
+            // so we have to enforce params.max_var_teams
             //
-            // C.total_flops is < group0.flops,
+            // C.total_flops is < team0.flops,
             // else we would have already accepted it
             //
             while (1) {
-                double x = c.total_flops + h->flops;
-                if (x > max_group_flops
-                    || (!params.aggressive && x > s.flops0*params.max_var_groups)
+                double x = c.total_flops + h.flops;
+                if (x > max_team_flops
+                    || (!params.aggressive && x > league.flops0*params.max_var_teams)
                 ) {
                     // adding H could make C too fast;
                     // see if we can accept C as is
@@ -279,7 +281,7 @@ void find_group_set3(
                     if (c.prune_inner_storage(params.size_inner)) {
                         continue;
                     }
-                    if (c.total_flops < s.flops0/params.max_var_groups) {
+                    if (c.total_flops < league.flops0/params.max_var_teams) {
                         // C is too slow to accept, but adding H
                         // would make it too fast.
                         // Remove the first host from C and see if this
@@ -288,13 +290,13 @@ void find_group_set3(
                         c.remove_first();
                         continue;
                     }
-                    // accept C; the new candidate group is {H}
+                    // accept C; the new candidate team is {H}
                     //
 #ifdef DEBUG
-                    printf("add group 4\n");
+                    printf("add team 4\n");
                     c.print();
 #endif
-                    s.add_group(c);
+                    league.add_team(c);
                     c.clear();
                     c.add_host(h);
                     goto next_host;
@@ -302,7 +304,7 @@ void find_group_set3(
                     // adding H to C would not make it too fast
                     // See if H is too slow for C
                     //
-                    if (h->flops < c.max_flops/params.max_var_hosts) {
+                    if (h.flops < c.max_flops/params.max_var_hosts) {
                         // Yes; try to accept C
                         if (c.prune_inner_storage(params.size_inner)) {
                             continue;
@@ -314,15 +316,15 @@ void find_group_set3(
                             c.add_host(h);
                             goto next_host;
                         }
-                        if (c.total_flops < s.flops0/params.max_var_groups) {
+                        if (c.total_flops < league.flops0/params.max_var_teams) {
                             c.remove_first();
                             continue;
                         }
 #ifdef DEBUG
-                        printf("add group 5\n");
+                        printf("add team 5\n");
                         c.print();
 #endif
-                        s.add_group(c);
+                        league.add_team(c);
                         c.clear();
                         c.add_host(h);
                         goto next_host;
@@ -333,29 +335,29 @@ void find_group_set3(
                         goto next_host;
                     }
 
-                    // if C is at least s.flops0, accept it
+                    // if C is at least league.flops0, accept it
                     //
-                    if (c.total_flops >= s.flops0) {
+                    if (c.total_flops >= league.flops0) {
 #ifdef DEBUG
-                        printf("add group 6\n");
+                        printf("add team 6\n");
                         c.print();
 #endif
-                        s.add_group(c);
+                        league.add_team(c);
                         c.clear();
                         goto next_host;
                     }
 
-                    if (c.hosts.size() == params.max_hosts_per_group) {
+                    if (c.hosts.size() == params.max_hosts_per_team) {
                         // C is now full.  See if it's fast enough to accept
                         //
-                        if (c.total_flops > s.flops0/params.max_var_groups) {
+                        if (c.total_flops > league.flops0/params.max_var_teams) {
                             // yes - accept C
                             //
 #ifdef DEBUG
-                            printf("add group 7\n");
+                            printf("add team 7\n");
                             c.print();
 #endif
-                            s.add_group(c);
+                            league.add_team(c);
                             c.clear();
                             goto next_host;
                         } else {
@@ -371,77 +373,75 @@ void find_group_set3(
 next_host: ;
     }
 done:
-    // see if the candidate group is acceptable
+    // see if the candidate team is acceptable
     c.prune_inner_storage(params.size_inner);
-    if (c.total_flops > s.flops0/params.max_var_groups) {
+    if (c.total_flops > league.flops0/params.max_var_teams) {
 #ifdef DEBUG
-        printf("add group 8\n");
+        printf("add team 8\n");
         c.print();
 #endif
-        s.add_group(c);
+        league.add_team(c);
     }
 #ifdef DEBUG
-    printf("find_group_set3() done:\n");
-    s.print();
+    printf("find_league3() done:\n");
+    league.print();
 #endif
 }
 
-// Find a group set with the constraint that
-// group average can't exceed a given value.
+// Find a league with the constraint that
+// team average can't exceed a given value.
 //
-void find_group_set2(
-    vector<HOST*> &hosts, JOB_PARAMS &params, GROUP_SET &gs,
-    double max_group_flops
+void find_league2(
+    vector<HOST*> &hosts, JOB_PARAMS &params, LEAGUE &league,
+    double max_team_flops
 ) {
 #ifdef DEBUG
-    printf("find_group_set2() start: max_group_flops %f\n", max_group_flops);
+    printf("find_league2() start: max_team_flops %f\n", max_team_flops);
 #endif
-    for (int i=params.max_groups; i>0; i--) {
+    for (int i=params.max_teams; i>0; i--) {
         double min_outer_storage = params.size_outer/i;
-        find_group_set3(
-            hosts, params, gs, max_group_flops, min_outer_storage
+        find_league3(
+            hosts, params, league, max_team_flops, min_outer_storage
         );
-        if (gs.groups.size() >= i) {
+        if (league.teams.size() >= i) {
 #ifdef DEBUG
-            printf("find_group_set2(): success\n");
-            gs.print();
+            printf("find_league2(): success\n");
+            league.print();
 #endif
             return;
         }
     }
     // nothing worked
 #ifdef DEBUG
-    printf("find_group_set2(): fail\n");
+    printf("find_league2(): fail\n");
 #endif
-    gs.clear();
+    league.clear();
 }
 
-// Find a group set.
-// Explore a range of values for max group flops
+// Find a league.
+// Explore a range of values for max team flops
 // Start at the largest possible value (sum of top N hosts)
 // and go down by powers of two.
 // Keep going until total FLOPs decreases
 //
-void find_group_set(
-    vector<HOST*> &hosts, JOB_PARAMS &params, GROUP_SET &gs
+void find_league(
+    vector<HOST*> &hosts, JOB_PARAMS &params, LEAGUE &league
 ) {
     sort(hosts.begin(), hosts.end(), compare);
-    double max_group_flops = hosts[0]->flops*params.max_hosts_per_group;
+    double max_team_flops = hosts[0]->flops*params.max_hosts_per_team;
     double best = 0;
     while (1) {
-        GROUP_SET gs2;
-        find_group_set2(
-            hosts, params, gs2, max_group_flops
-        );
+        LEAGUE lg2;
+        find_league2(hosts, params, lg2, max_team_flops);
 #ifdef DEBUG
-        printf("find_group_set: solution %f best %f\n", gs2.total_flops, best);
+        printf("find_league: solution %f best %f\n", lg2.total_flops, best);
 #endif
-        if (gs2.total_flops <= best) {
+        if (lg2.total_flops <= best) {
             break;
         }
-        best = gs2.total_flops;
-        max_group_flops /= 2;
-        gs = gs2;
+        best = lg2.total_flops;
+        max_team_flops /= 2;
+        league = lg2;
     }
 }
 
@@ -467,17 +467,17 @@ int main(int, char**) {
     hosts.push_back(new HOST(.2, 10, 10, id++));
     hosts.push_back(new HOST(.2, 10, 10, id++));
 
-    params.max_hosts_per_group = 20;
-    params.max_groups = 4;
+    params.max_hosts_per_team = 20;
+    params.max_teams = 4;
     params.max_var_hosts = 1.5;
-    params.max_var_groups = 1.4;
+    params.max_var_teams = 1.4;
     params.size_outer = 10;
     params.size_inner = 10;
     params.aggressive = false;
 
-    GROUP_SET gs;
-    //find_group_set(hosts, params, gs);
-    find_group_set3(hosts, params, gs, 1.5, 0);
-    gs.print();
+    LEAGUE league;
+    //find_league(hosts, params, league);
+    find_league3(hosts, params, league, 1.5, 0);
+    league.print();
 
 }
